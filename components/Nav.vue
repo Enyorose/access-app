0ba1d700aacb5d444c68403a497f40b6be83491c
Evan Burton <butonevan1@gmaail.com>
2021-12-08 14:39:01 -0700

    tweak

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,13 +1,12 @@
 <template>
   <v-container>
     <div>
-  <ul>
-      <li><NuxtLink to="/">Home</NuxtLink></li>
-      <li><NuxtLink to="/">Services</NuxtLink></li>
-      <li><NuxtLink to="/">Contact</NuxtLink></li>
-      <li><NuxtLink to="/">Community</NuxtLink></li>
-      
-    </ul>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/">Services</NuxtLink></li>
+        <li><NuxtLink to="/">Contact</NuxtLink></li>
+        <li><NuxtLink to="/">Community</NuxtLink></li>
+      </ul>
     </div>
   </v-container>
 </template>
