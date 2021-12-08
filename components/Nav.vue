a6b4288c12763f6c9c62fef0a94803078b2335a9
Evan Burton <butonevan1@gmaail.com>
2021-12-08 13:21:39 -0700

    update to nav links

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,7 +1,13 @@
 <template>
   <v-container>
     <div>
-
+  <ul>
+      <li><NuxtLink to="/">Home</NuxtLink></li>
+      <li><NuxtLink to="/">Services</NuxtLink></li>
+      <li><NuxtLink to="/">Contact</NuxtLink></li>
+      <li><NuxtLink to="/">Community</NuxtLink></li>
+      
+    </ul>
     </div>
   </v-container>
 </template>
