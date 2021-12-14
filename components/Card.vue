ae6460b4c2a28b09767e1cc6b880f04236a0bce0
Jessica.W <jessica.watson1@live.com>
2021-12-13 18:45:59 -0700

    finished up cards

--- components/Card.vue
+++ components/Card.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <figure>
-      <img :src="place.image_url" alt="" />
-      <p>{{ place.name }}</p>
+      <h3>{{ place.name }}</h3>
+      <img :src="place.image_url" :alt="place.alias" width="150" height="150" />
+      <figcaption></figcaption>
+      <a :href="place.url"><button>More on your new favorite</button></a>
     </figure>
   </div>
 </template>
