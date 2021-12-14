5ce53c9ed55794448883782664d619ae36d9e9d5
Jessica.W <jessica.watson1@live.com>
2021-12-14 10:10:53 -0700

    added page data

--- pages/community.vue
+++ pages/community.vue
@@ -1,10 +1,20 @@
 <template>
-<main>
-  <div>
-    <Header />
-  </div>
-  <Community />
-
-</main>
+  <main>
+    <div>
+      <Header />
+    </div>
+    <Community />
+  </main>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      pageInfo: {
+        menuName: "community",
+        header: "Community",
+      },
+    };
+  },
+};
+</script>
