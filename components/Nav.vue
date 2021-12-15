26c9c541be3be4c0ae6db43b9372332c40ee741e
Jessica.W <jessica.watson1@live.com>
2021-12-15 10:21:14 -0700

    fixed nav colors

--- components/Nav.vue
+++ components/Nav.vue
@@ -8,39 +8,21 @@
         <li>
           <NuxtLink to="/services">Services</NuxtLink>
         </li>
-         <li>
+        <li>
           <NuxtLink to="/contact">Contact</NuxtLink>
         </li>
-         <li>
+        <li>
           <NuxtLink to="/community">Community</NuxtLink>
         </li>
       </ul>
     </nav>
     <main>
-      
       <Nuxt />
     </main>
   </div>
 </template>
 <script>
-<<<<<<< HEAD
-export default {
-  props: ["pageInfo"],
-  data() {
-    return {
-      // Custom page data comes here.
-      navClass: {
-        active:
-          "font-extrabold text-center block border-2 border-gray-300 rounded py-2 px-4 bg-emerald-500 text-indigo-600",
-        inactive:
-          "font-semibold text-center block border-2 border-gray-300 rounded py-2 px-4 text-blue-300 bg-white hover:text-indigo-600 hover:bg-teal-200",
-      },
-    };
-  },
-};
-=======
 export default {};
->>>>>>> 7044c82c01b0a41f74f94d80956932fbb9c87349
 </script>
 
 <style lang="scss">
@@ -49,15 +31,14 @@ export default {};
 @import url("https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap");
 /* home route and active route will show in bold as it matches / and /about */
 a.nuxt-link-active {
-  font-weight: bold;
 }
 /* exact link will show the primary color for only the exact matching link */
 a.nuxt-link-exact-active {
+  // color: #00c58e;
   color: #00c58e;
 }
 
 body {
-  
   margin: 0;
 }
 
@@ -73,10 +54,7 @@ img {
 }
 
 ul {
-<<<<<<< HEAD
   font-family: "Kosugi Maru", sans-serif;
-=======
-    font-family: "Kosugi Maru", sans-serif;
   list-style-type: none;
   padding: 0;
   display: flex;
@@ -84,31 +62,30 @@ ul {
   flex-wrap: wrap;
 }
 li {
-  background-color: teal;
+  // background-color: #fdd1c7;
+  background-color: grey;
   border: solid;
   border-radius: 10px;
-  border-color: indigo;
+  // border-color: #cd8388;
+  border-color: lightgrey;
   margin: 0 0.5rem;
   padding: 0.25rem;
   font-size: 1.2rem;
-  
 }
 
 nav {
   padding: 0 1rem;
->>>>>>> 7044c82c01b0a41f74f94d80956932fbb9c87349
 }
 
 a,
 a:visited {
   text-decoration: none;
-  color: indigo;
+  // color: #7e372a;
+  color: black;
 }
 
 a:hover {
-  color: #00c58e;
+  // color: #35645d;
+  color: white;
 }
-
-
-
 </style>
